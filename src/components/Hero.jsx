154ea96c5ab8905ca0cc73e812ea239c0158836c
Aditya Phasu <adityaphasu919@gmail.com>
2025-03-06 21:27:25 +0530

    add files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <section className="pt-52 w-[95%] mx-auto">
+      <div className="text-center lg:text-left">
+        <h1 className=" uppercase font-bold text-[3.125rem] drop-shadow">Fund. Build. Scale.</h1>
+        <h2 className=" text-xl">Unlock Web3 Grants & Bring yout vision to life.</h2>
+        <p className="my-1.5 max-w-[45rem] mx-auto lg:mx-0">
+          We have secured over €3 million in grants, bringing more than seven years of experience in
+          Web3. With a proven track record, we have successfully launched and managed over 20 funded
+          projects and two grant programs.
+        </p>
+      </div>
+    </section>
+  );
+};
+export default Hero;
