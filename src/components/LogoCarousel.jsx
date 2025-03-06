9df95e04d0f006cf364b0335470d1cb55581ca31
Aditya Phasu <adityaphasu919@gmail.com>
2025-03-07 03:24:52 +0530

    add logo carousel component and SVG assets

---
+++ src/components/LogoCarousel.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import algorand from "../assets/algorand.svg";
+import cosmos from "../assets/cosmos.svg";
+import celo from "../assets/celo.svg";
+import ethereum from "../assets/ethereum.svg";
+import europeanCommission from "../assets/european-commission.svg";
+import filecoin from "../assets/filecoin.svg";
+import ripple from "../assets/ripple.svg";
+import stellar from "../assets/stellar.svg";
+
+const logos = [algorand, cosmos, celo, ethereum, filecoin, ripple, stellar, europeanCommission];
+
+const LogoCarousel = () => {
+  useEffect(() => {
+    const carousel = document.querySelector(".carousel");
+    const carouselTrack = document.querySelector(".carousel-track");
+    const clone = carouselTrack.cloneNode(true);
+    carousel.appendChild(clone);
+  }, []);
+
+  return (
+    <div className="carousel relative flex overflow-hidden mx-20 py-10">
+      <div className="carousel-track flex py-3 items-center ">
+        {logos.map((logo) => (
+          <div className="w-48 h-14 flex items-center justify-center mx-5" key={logo}>
+            <img src={logo} alt="logo" className="w-full" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LogoCarousel;
