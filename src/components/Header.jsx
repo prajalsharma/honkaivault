fe8c826baf8072594a4f9f7fd100dc41f577647b
Aditya Phasu <adityaphasu919@gmail.com>
2025-03-08 21:30:03 +0530

    refactor Header and Hero components for improved layout and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,23 @@
 const Header = () => {
   return (
-    <header className="px-5 py-4 text-white fixed w-full border-b border-primary-gray rounded-2xl lg:border-none backdrop-blur-lg z-50">
+    <header className="px-5 py-4 text-white fixed w-full border-b border-primary-gray rounded-2xl lg:border-none backdrop-blur-lg z-50 md:px-8">
       <nav className="flex flex-col md:flex-row items-center justify-between text-sm gap-3  ">
         <div className="w-72 lg:w-60">
           <a href="https://www.cornarolabs.xyz">
             <img src="./logo.png" alt="" />
           </a>
         </div>
-        <div className="flex items-center gap-10">
-          <ul className="flex items-center gap-10">
-            <li className="relative group">
-              <a href="#services">Services</a>
-              <span className="w-full h-0.5 bg-white block absolute transition-all opacity-0 top-7 group-hover:opacity-100 group-hover:top-5" />
-            </li>
-            <li className="relative group">
-              <a href="#about-us">About Us</a>
-              <span className="w-full h-0.5 bg-white block absolute transition-all opacity-0 top-7 group-hover:opacity-100 group-hover:top-5" />
-            </li>
-          </ul>
 
-          <div className="flex items-center gap-2">
-            <a className="custom-btn" href="https://www.web3grants.co/">
-              <span className="text">Grants DashBoard</span>
-            </a>
-            <a className="custom-btn" href="https://calendly.com/cornarolabs">
-              <span className="text">Consultation</span>
-            </a>
-          </div>
-        </div>
+        <ul className="flex items-center gap-10">
+          <li className="relative group">
+            <a href="#services">Services</a>
+            <span className="w-full h-0.5 bg-white block absolute transition-all opacity-0 top-7 group-hover:opacity-100 group-hover:top-5" />
+          </li>
+          <li className="relative group">
+            <a href="#about-us">About Us</a>
+            <span className="w-full h-0.5 bg-white block absolute transition-all opacity-0 top-7 group-hover:opacity-100 group-hover:top-5" />
+          </li>
+        </ul>
       </nav>
     </header>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,18 +2,27 @@ import LogoCarousel from "./LogoCarousel";
 
 const Hero = () => {
   return (
-    <section className="hero pt-52 pb-36 relative">
-      <div className="flex flex-col gap-28 px-9">
-        <div className="text-center lg:text-left">
+    <section className="hero pt-52 md:pb-36 pb-16 relative overflow-hidden">
+      <div className="text-box flex flex-col gap-4 px-10 relative z-40">
+        <div className="text-center lg:text-left z-10">
           <h1 className=" uppercase font-bold text-[3.125rem] drop-shadow">Fund. Build. Scale.</h1>
-          <h2 className=" text-xl">Unlock Web3 Grants & Bring yout vision to life.</h2>
+          <h2 className=" text-xl">Unlock Web3 Grants & Bring your vision to life.</h2>
           <p className="my-1.5 max-w-[45rem] mx-auto lg:mx-0">
             We have secured over €3 million in grants, bringing more than seven years of experience
             in Web3. With a proven track record, we have successfully launched and managed over 20
             funded projects and two grant programs.
           </p>
         </div>
+        <div className="btn-box flex items-center gap-2 flex-wrap justify-center lg:justify-start z-40 relative">
+          <a className="custom-btn" href="https://www.web3grants.co/">
+            <span className="text">Web3 Grants DashBoard</span>
+          </a>
+          <a className="custom-btn" href="https://calendly.com/cornarolabs">
+            <span className="text">Book a free Consultation</span>
+          </a>
+        </div>
       </div>
+      <div className="shape-1 absolute " />
     </section>
   );
 };
