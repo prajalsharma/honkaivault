62c8b4e118fb82a0a366a6f70f1becb6a3d8e3f4
DarthPrajal <prajalsharma1120@gmail.com>
2025-03-12 16:24:56 +0530

    Update LogoCarousel.jsx

--- src/components/LogoCarousel.jsx
+++ src/components/LogoCarousel.jsx
@@ -8,7 +8,17 @@ import filecoin from "../assets/filecoin.svg";
 import ripple from "../assets/ripple.svg";
 import stellar from "../assets/stellar.svg";
 
-const logos = [algorand, cosmos, celo, ethereum, filecoin, ripple, stellar, europeanCommission];
+// Array of logos with corresponding company links
+const logos = [
+  { src: algorand, url: "https://www.algorand.com/" },
+  { src: cosmos, url: "https://cosmos.network/" },
+  { src: celo, url: "https://celo.org/" },
+  { src: ethereum, url: "https://ethereum.org/" },
+  { src: filecoin, url: "https://filecoin.io/" },
+  { src: ripple, url: "https://ripple.com/" },
+  { src: stellar, url: "https://www.stellar.org/" },
+  { src: europeanCommission, url: "https://ec.europa.eu/" }
+];
 
 const LogoCarousel = () => {
   useEffect(() => {
@@ -20,10 +30,12 @@ const LogoCarousel = () => {
 
   return (
     <div className="carousel relative flex overflow-hidden mx-5 md:mx-20 py-10">
-      <div className="carousel-track flex py-3 items-center ">
+      <div className="carousel-track flex py-3 items-center">
         {logos.map((logo) => (
-          <div className="w-48 h-14 flex items-center justify-center mx-5" key={logo}>
-            <img src={logo} alt="logo" className="w-full" />
+          <div className="w-48 h-14 flex items-center justify-center mx-5" key={logo.url}>
+            <a href={logo.url} target="_blank" rel="noopener noreferrer">
+              <img src={logo.src} alt="logo" className="w-full hover:opacity-80 transition-opacity duration-200" />
+            </a>
           </div>
         ))}
       </div>
