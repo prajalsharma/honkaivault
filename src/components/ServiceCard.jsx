cfa65ac83d17effb6a5b3e804988175ec5fecddd
Aditya Phasu <adityaphasu919@gmail.com>
2025-03-14 15:40:39 +0530

    Add ServiceCard component with animated toggle functionality and SVG asset

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,22 +1,44 @@
-import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "./ui/dialog";
-import { ScrollArea } from "./ui/scroll-area";
+import { AnimatePresence, motion } from "motion/react";
+import { ChevronDown } from "lucide-react";
+import { useState } from "react";
 
 const ServiceCard = ({ service }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <div className="service-card text-white text-base flex flex-col cursor-pointer gap-4 p-4  rounded-lg h-full items-center justify-center font-bold bg-[#171b30]/75 hover:bg-[#171b30]/50 transition-colors text-center relative pt-16 pb-8">
-          <div className="absolute -top-8  text-white service-icon p-2.5">{service.icon}</div>
-          <h3>{service.title}</h3>
-          <p className="text-sm text-primary-gray font-normal">{service.summary}</p>
-        </div>
-      </DialogTrigger>
-      <DialogContent className="bg-[#000612] text-white/70 text-sm">
-        <DialogTitle className="text-2xl text-white underline">{service.title}</DialogTitle>
-        <DialogDescription className="sr-only">{service.tile}</DialogDescription>
-        <ScrollArea className="max-h-[400px] whitespace-pre-wrap">{service.description}</ScrollArea>
-      </DialogContent>
-    </Dialog>
+    <motion.button
+      layout
+      onClick={() => setIsOpen(!isOpen)}
+      className="service-card bg-[#171b30]/75 hover:bg-[#171b30]/50 rounded-lg cursor-pointer flex flex-col justify-center  group pb-2 min-h-[15rem] transition-colors">
+      <motion.div
+        layout
+        className=" text-white text-base flex flex-col gap-4 p-4 h-full items-center justify-center font-bold transition-colors text-center relative pt-16 ">
+        <div className="absolute -top-8  text-white service-icon p-2.5">{service.icon}</div>
+        <h3>{service.title}</h3>
+        <p className="text-sm text-primary-gray font-normal">{service.summary}</p>
+      </motion.div>
+      <div>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.75, ease: "easeInOut" }}
+              className="text-white flex flex-col gap-4 p-4 h-full items-center justify-center text-sm relative">
+              {service.description}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      <motion.div layout className="flex justify-center">
+        <ChevronDown
+          className={`text-white group-hover:text-[#00b9fa] size-8 transition-all ${
+            isOpen ? "rotate-180" : ""
+          }`}
+        />
+      </motion.div>
+    </motion.button>
   );
 };
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,6 @@
-import { motion } from "motion/react";
-import { ArrowUpRight, Bitcoin, ClipboardList, Coins, Rocket } from "lucide-react";
+import { LayoutGroup, motion } from "motion/react";
+import { ArrowUpRight, Bitcoin, ClipboardList, Diamond, Rocket } from "lucide-react";
+import Cube from "../assets/cube.svg";
 import ServiceCard from "./ServiceCard";
 import { gradientButtonVariants } from "@/lib/animations";
 
@@ -8,33 +9,100 @@ const services = [
     title: "Funding & Grant Opportunities",
     summary:
       "Leveraging our extensive experience with various blockchain protocols, and various European funding initiatives, we excel in securing funding for innovative projects.",
-    icon: <Coins className="size-14" />,
-    description:
-      "Leveraging our extensive experience with various blockchain protocols and European funding initiatives, we excel in securing funding for innovative projects. We assist you in identifying funding opportunities, crafting compelling applications, and finding strategic partners when necessary. Our expertise in the blockchain sector and European research & innovation projects, combined with a proven track record of raising over €3M+ in recent years, enables us to offer high-quality, customized services. Our goal is to empower our clients to seize new funding opportunities and transform innovative ideas into successfully funded ventures.",
+    icon: <img src={Cube} alt="" className="size-14" />,
+    description: (
+      <p className=" !text-white font-normal text-justify">
+        We assist you in identifying funding opportunities, crafting compelling applications and
+        finding strategic partners when necessary. Our seasoned expertise in the blockchain sector
+        and European research & innovation projects, combined with a proven track record of raising
+        over €3M+ in recent years only, enables us to offer high-quality, customized services. Our
+        goal is to empower our clients to seize new funding opportunities and transform innovative
+        ideas into successfully funded ventures.
+      </p>
+    ),
   },
   {
     title: "Project Management & Research",
     summary:
       "We provide comprehensive project management and research services across institutional, industry, and blockchain ecosystems. ",
     icon: <ClipboardList className="size-14" />,
-    description:
-      "We provide comprehensive project management and research services across institutional, industry, and blockchain ecosystems. We have supported projects funded by the European Commission, Layer 1 blockchain protocols like Cosmos and Ethereum, and other decentralized initiatives. \nOur expertise covers regulatory compliance, multi-partner coordination, blockchain, cybersecurity, and policy research with in-depth whitepapers and reports. \nOur portfolio includes blockchain ecosystem analyses, metaverse insights, EU policy consultations, and cybersecurity evaluations. \nWe have a strong track record of successfully managing large-scale, multi-stakeholder projects while ensuring strategic alignment, regulatory adherence, and impactful research.",
+    description: (
+      <div className="flex flex-col gap-3 text-justify">
+        <p className="!text-white">
+          We have supported projects funded by the European Commission, Layer 1 blockchain protocols
+          like Cosmos and Ethereum, and other decentralized initiatives.
+        </p>
+        <ul className="text-white  space-y-2">
+          <li className="flex gap-3 items-center">
+            <Diamond className="size-3 shrink-0" /> Expertise in regulatory compliance,
+            multi-partner coordination, and execution.
+          </li>
+          <li className="flex gap-3 items-center">
+            <Diamond className="size-3 shrink-0" /> Blockchain, cybersecurity, and policy research
+            with in-depth whitepapers and reports.
+          </li>
+          <li className="flex gap-3 items-center">
+            <Diamond className="size-3 shrink-0" /> Strong portfolio covering blockchain ecosystem
+            analyses, metaverse insights, EU policy consultations, and cybersecurity evaluations .
+          </li>
+        </ul>
+        <p className="!text-white">
+          Our team has a proven track record of successfully managing large-scale, multi-stakeholder
+          projects while ensuring strategic alignment, regulatory adherence, and impactful research.
+        </p>
+      </div>
+    ),
   },
   {
     title: "MVP Development – Need a Web3 Developer?",
     summary:
       "Looking to build your Web3 idea but don’t have a developer? We connect you with skilled Web3 builders to help you.",
     icon: <Rocket className="size-14" />,
-    description:
-      "We offer: \nYou with skilled Web3 builders to help you develop your Minimum Viable Product (MVP). \nOptimize smart contracts & blockchain infrastructure. \nBring your Web3 vision to life with expert development support.",
+    description: (
+      <div className="text-white flex flex-col gap-3 text-left">
+        <span>We offer:</span>
+        <ul className="text-white space-y-2">
+          <li className="flex gap-3 items-center">
+            <Diamond className="size-3 shrink-0" /> Develop your MVP (Minimum Viable Product).
+          </li>
+          <li className="flex gap-3 items-center">
+            <Diamond className="size-3 shrink-0" /> Optimize smart contracts & blockchain
+            infrastructure.
+          </li>
+          <li className="flex gap-3 items-center">
+            <Diamond className="size-3 shrink-0" />
+            Bring your Web3 vision to life with expert dev support.
+          </li>
+        </ul>
+      </div>
+    ),
   },
   {
     title: "Web3 & Blockchain Consulting",
     summary:
       "Strategic guidance for Web2 businesses entering Web3 and established blockchain projects.",
     icon: <Bitcoin className="size-14" />,
-    description:
-      "From Web2 businesses transitioning into Web3 to established blockchain projects refining their tokenomics, we provide strategic consulting. \nOur services include use case identification, platform selection, and implementation strategies for new entrants.\nScaling operations, smart contract optimization, and compliance guidance for existing Web3 projects.",
+    description: (
+      <div className="text-white flex flex-col gap-3 text-left">
+        <span>We offer:</span>
+        <ul className="text-white space-y-2">
+          <li className="flex gap-3 items-center">
+            <Diamond className="size-3 shrink-0" />{" "}
+            <span>
+              <span className="font-bold">New Entrants:</span>
+              Use case identification, platform selection, and implementation strategies.
+            </span>
+          </li>
+          <li className="flex gap-3 items-center">
+            <Diamond className="size-3 shrink-0" />
+            <span>
+              <span className="font-bold">Existing Web3 Projects:</span> Scaling operations, smart
+              contract optimization, and compliance guidance
+            </span>
+          </li>
+        </ul>
+      </div>
+    ),
   },
 ];
 
@@ -61,48 +129,53 @@ const serviceCardVariants = {
 
 const Services = () => {
   return (
-    <section id="services" className="py-8 lg:py-20 px-10">
-      <motion.h2
-        variants={fadeInVariant}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        className="text-5xl text-white font-bold text-center uppercase">
-        Services
-      </motion.h2>
-      <motion.div
-        variants={container}
-        initial="initial"
-        whileInView="animate"
-        viewport={{ once: true, amount: 0.5 }}
-        className="grid grid-cols-1 lg:grid-cols-2 gap-20 lg:gap-16 py-20 lg:px-20">
-        {services.map((service, index) => (
-          <motion.div variants={serviceCardVariants} key={index}>
-            <ServiceCard service={service} />
+    <LayoutGroup>
+      <section id="services" className="py-8 lg:py-20 px-10">
+        <motion.h2
+          variants={fadeInVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          className="text-5xl text-white font-bold text-center uppercase">
+          Services
+        </motion.h2>
+        <motion.div layout>
+          <motion.div
+            variants={container}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true, amount: 0.5 }}
+            layout
+            className="grid grid-cols-1 lg:grid-cols-2 gap-20 lg:gap-16 py-20 lg:px-20 auto-rows-[minmax(200px,auto)]">
+            {services.map((service, index) => (
+              <motion.div variants={serviceCardVariants} key={index}>
+                <ServiceCard service={service} />
+              </motion.div>
+            ))}
           </motion.div>
-        ))}
-      </motion.div>
-      <motion.div
-        variants={fadeInVariant}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        className="max-w-min mx-auto">
-        <motion.a
-          variants={gradientButtonVariants}
-          initial="initial"
-          whileHover="hover"
-          className="custom-btn-2 group"
-          href="https://calendly.com/cornarolabs"
-          target="_blank"
-          rel="noreferrer">
-          <span className="flex items-center gap-2">
-            Discover How We Can Help{" "}
-            <ArrowUpRight className="transition-transform group-hover:translate-x-1 group-hover:-translate-y-1.5 duration-500" />
-          </span>
-        </motion.a>
-      </motion.div>
-    </section>
+          <motion.div
+            variants={fadeInVariant}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            className="max-w-min mx-auto">
+            <motion.a
+              variants={gradientButtonVariants}
+              initial="initial"
+              whileHover="hover"
+              className="custom-btn-2 group"
+              href="https://calendly.com/cornarolabs"
+              target="_blank"
+              rel="noreferrer">
+              <span className="flex items-center gap-2">
+                Discover How We Can Help{" "}
+                <ArrowUpRight className="transition-transform group-hover:translate-x-1 group-hover:-translate-y-1.5 duration-500" />
+              </span>
+            </motion.a>
+          </motion.div>
+        </motion.div>
+      </section>
+    </LayoutGroup>
   );
 };
 export default Services;
